e19237363fe4782759886e5afc26d0d70d711f85
Aleksandr Lavrov <lavrovaleksandr.work@gmail.com>
2025-10-06 17:55:43 +0300

    Added an html reporter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
   verbose: true,
+  reporters: ['default', [
+    './node_modules/jest-html-reporter',
+    {
+      pageTitle: 'Test Report',
+      outputPath: 'reports/test-report.html',
+      includeFailureMsg: true,
+      includeSuiteFailure: true,
+    }
+  ]]
 };
